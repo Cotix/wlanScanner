7658f1b994923888227ea595af19a88239a1fd7e
Erik Gaal <git@erikgaal.nl>
2015-04-01 13:41:13 +0200

    Drawing shit on map

--- wlanScan/src/GUI/Viewer.java
+++ wlanScan/src/GUI/Viewer.java
@@ -6,16 +6,17 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 
 import Utils.Position;
+import Utils.Utils;
 
 /**
  * Shows the map of Spiegel with the current location drawn on it
@@ -23,9 +24,11 @@ import Utils.Position;
  *
  */
 public class Viewer implements Runnable {
-	
+
+	private Set<int[]> points;
+
 	private LinkedBlockingQueue<Position> pos;
-	
+
 	public Viewer(LinkedBlockingQueue<Position> pos){
 		this.pos = pos;
 	}
@@ -47,7 +50,7 @@ public class Viewer implements Runnable {
 			panel.add(label);
 			frame.add(panel);
 			frame.setVisible(true);
-			
+
 			Graphics draw = image.getGraphics();
 			draw.setColor(Color.RED);
 
@@ -57,6 +60,13 @@ public class Viewer implements Runnable {
 				currentPos = getMapPosition(currentPos);
 				draw.drawImage(backup, 0, 0, null);
 				draw.drawImage(point, (int)currentPos.getX()-5, (int)currentPos.getY()-5, null);
+
+				for (int[] p : points) {
+					Position ppos = getMapPosition(new Position(p[0], p[1]));
+					int radius = (p[2] * 10);
+					draw.drawOval((int) (ppos.getX()-radius/2)-5, (int) (ppos.getY()-radius/2)-5, radius, radius);
+				}
+
 				label.repaint();
 			}
 
@@ -65,7 +75,16 @@ public class Viewer implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void updatePoints(final HashSet<int[]> update) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				points = update;
+			}
+		});
+	}
+
 	/**
 	 * Translates from map grid coordinates to pixel coordinates used for drawing
 	 * @param pos
@@ -75,11 +94,11 @@ public class Viewer implements Runnable {
 		double x = pos.getX();
 		x = x*10;
 		x = x + 45;
-		
+
 		double y = pos.getY();
 		y = y*10;
 		y = y + 44;
-		
+
 		return new Position(x,y);
 	}
 }
